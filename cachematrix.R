05d89db7f4acd5bf106013180c9a89811efa32bf
pwohl <pwohland@gmail.com>
2014-04-27 08:13:15 +0100

    This is a pair of functions
    This is a pair of functions to cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -29,13 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ##the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         
-        inv <- x$getinverse()
+        inv <- x$getinvers()
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
         }
         data <- x$get()
         inv <- solve(data, ...)
-        x$setinverse(inv)
+        x$setinvers(inv)
         inv
 }
